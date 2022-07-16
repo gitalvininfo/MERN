c30cfee034f5231707f236fecd58a1d4e4724e64
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-16 21:10:50 +0800

    done in controller update and delete

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,28 +1,22 @@
 const express = require('express');
-const { createWorkout, getWorkouts, getWorkout } = require('../controllers/workoutController');
+const { createWorkout, getWorkouts, getWorkout, updateWorkout, deleteWorkout } = require('../controllers/workoutController');
 
 const router = express.Router();
+
 // GET all workouts
 router.get("/", getWorkouts)
-
 
 // GET a single workout
 router.get("/:id", getWorkout)
 
-
 // POST a new workout
 router.post("/", createWorkout)
 
-
 // DELETE a workout
-router.delete("/:id", (req, res) => {
-    res.json({ mssg: "DELETE a workout" })
-})
+router.delete("/:id", deleteWorkout)
 
 // UPDATE a workout
-router.patch("/:id", (req, res) => {
-    res.json({ mssg: "UPDATE a new workout" })
-})
+router.patch("/:id", updateWorkout)
 
 
 
