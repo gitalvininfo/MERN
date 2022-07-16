1ad818df902182495210d51c0d6ca351a6b676a2
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-16 20:16:28 +0800

    mongdb atlast setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // express app
@@ -13,9 +14,17 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port 4000`)
-})
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to db and listening on port 4000`)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
