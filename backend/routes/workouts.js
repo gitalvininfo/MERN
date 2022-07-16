945a6008a62b92ef2b0c6db65aa92bf17a5e04cd
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-16 20:35:19 +0800

    done in models and schemas

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const Workout = require('../models/workoutModel');
 
 const router = express.Router();
-
 // GET all workouts
 router.get("/", (req, res) => {
     res.json({ mssg: "GET all workouts" })
@@ -15,8 +15,16 @@ router.get("/:id", (req, res) => {
 
 
 // POST a new workout
-router.post("/", (req, res) => {
-    res.json({ mssg: "POST a new workout" })
+router.post("/", async (req, res) => {
+    const { title, load, reps } = req.body;
+
+    try {
+        const workout = await Workout.create({ title, load, reps });
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
 })
 
 
