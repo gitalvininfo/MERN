8db53b9beba503d997cef4948e1566c7741bb80b
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-16 18:38:33 +0800

    express setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 
 // express app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// routes
+app.get('/', (req, res) => {
+    res.json({
+        mssg: "Welcome to the app"
+    })
+})
 
 // listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Listening on port 4000`)
 })
